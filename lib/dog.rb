948489630a93de2be768d165204437d0022f557b
Conor Hamilton <conhamil19@gmail.com>
2018-06-18 06:28:04 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  attr_accessor :id, :name, :breed 
+  
+  def initialize(id =  nil, name ,breed)
+    @id = id 
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF 
+    SQL
+end 
