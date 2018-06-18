1cb870eb966b05cab582cdaaf67850a894c670a7
Conor Hamilton <conhamil19@gmail.com>
2018-06-18 07:15:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,6 +59,13 @@ class Dog
     end
     dog
   end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
     
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
