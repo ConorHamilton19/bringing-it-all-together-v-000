4579be5e3cb4e3942792d69a0d1aa0c787bb7c28
Conor Hamilton <conhamil19@gmail.com>
2018-06-18 06:33:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   attr_accessor :id, :name, :breed 
   
-  def initialize(id:  nil, name: ,breed:)
+  def initialize(id: nil, name: ,breed:)
     @id = id 
     @name = name 
     @breed = breed 
@@ -21,5 +21,8 @@ class Dog
   def self.drop_table
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
+  end 
+  
+  def save 
   end 
 end 
